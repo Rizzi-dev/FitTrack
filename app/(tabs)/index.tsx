20db615e517fd845bb13babf9b2d2b855f60462a
Rizzi <gabrielrizzi333@gmail.com>
2024-11-12 21:48:35 -0300

    adição da tela de login

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,58 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, TextInput, Button, Alert, KeyboardAvoidingView, Platform } from 'react-native';
+import { useState } from 'react';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 
-export default function HomeScreen() {
+import users from '@/datas/users.json';
+
+interface User {
+  username: string;
+  password: string;
+}
+
+type RootStackParamList = {
+  Home: undefined;
+  MainDashboard: undefined;
+};
+
+type HomeScreenNavigationProp = NativeStackNavigationProp<
+  RootStackParamList,
+  'Home'
+>;
+
+const authenticateUser = (username: string, password: string): boolean => {
+  const user = (users as User[]).find(
+    (user) => user.username === username && user.password === password
+  );
+  return !!user;
+};
+
+interface HomeScreenProps {
+  navigation: HomeScreenNavigationProp;
+}
+
+export default function HomeScreen({ navigation }: HomeScreenProps) {
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  const handleLogin = () => {
+    if (authenticateUser(username, password)) {
+      Alert.alert('Login bem-sucedido!', 'Bem-vindo ao FitTrack!');
+      navigation.navigate('MainDashboard');
+    } else {
+      Alert.alert('Erro', 'Usuário ou senha incorretos');
+    }
+  };
+
   return (
+    <KeyboardAvoidingView
+      style={{flex: 1}}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      >
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
@@ -16,7 +62,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Bem vindo ao FitTrack</ThemedText>
+        <ThemedText type="title">Bem-vindo ao FitTrack</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
@@ -25,8 +71,26 @@ export default function HomeScreen() {
           Entre agora e transforme toda a sua rotina de treino com o FitTrack.
         </ThemedText>
       </ThemedView>
-      
+
+      <ThemedView style={styles.loginContainer}>
+        <ThemedText style={styles.stepContainer}>Faça seu login</ThemedText>
+        <TextInput
+          placeholder="Usuário"
+          value={username}
+          onChangeText={setUsername}
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Senha"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+          style={styles.input}
+        />
+        <Button title="Entrar" onPress={handleLogin} />
+      </ThemedView>
     </ParallaxScrollView>
+    </KeyboardAvoidingView>
   );
 }
 
@@ -38,7 +102,8 @@ const styles = StyleSheet.create({
   },
   stepContainer: {
     gap: 8,
-    marginBottom: 8,
+    marginBottom: 15,
+
   },
   reactLogo: {
     height: 500,
@@ -47,5 +112,22 @@ const styles = StyleSheet.create({
     top: 0,
     left: 0,
     position: 'absolute',
+  },
+  loginContainer: {
+    marginTop: 16,
+    padding: 16,
+    borderRadius: 8,
+    shadowColor: '#000',
+    shadowOpacity: 0.1,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+  },
+  input: {
+    color: '#fff',
+    height: 40,
+    borderColor: '#ccc',
+    borderWidth: 1,
+    marginBottom: 12,
+    paddingHorizontal: 8,
   },
 });
